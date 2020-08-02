c03cd3bdc903b5a8ec2a042e1f111c9845637883
George Talnagi <gtalnagi@yahoo.com>
2020-08-02 02:35:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@ def key_for_min_value(name_hash)
   min_value = nil 
   new_key = nil 
   name_hash.each do |key, value|
-  if min_value == nil || value < min_value
+  if min_value == 0 || value < min_value
     min_value = value 
     new_key = key 
   end
